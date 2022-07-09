fa371105702481972e17d93c631241d396972d5a
Leonardo Resilia <leonardodacostadasilva@hotmail.com>
2022-07-09 16:39:58 -0300

    style: estilização para aloxação dinamica das tasks

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-import { taskList } from "../utils/constants";
 import { TaskElment } from "./TaskElement";
 
 const TasksStyle = styled.div`
@@ -8,22 +7,21 @@ const TasksStyle = styled.div`
     flex-direction: column;
     justify-content: flex-start;
     align-items: center;
-    height:700px;
+    min-height:700px;
     width: 40%;
     background-color: rgb(38, 30, 45);
     border-radius: 20px;
     padding: 10px;
     margin-top: -20px;
-    max-height: fit-content;
+    height: 70%;
 
 
 `
 
-export function Tasks(){
-    const listRender = taskList.reverse()
+export function Tasks({tasks}){
     return (
         <TasksStyle>
-            {listRender.map((item, index)=>(
+            {tasks.map((item, index)=>(
                 <TaskElment key={index} newTask={item}/>
             ))}
         </TasksStyle>
